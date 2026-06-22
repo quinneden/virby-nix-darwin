4130d5b9570a5dfbb054cd9fc96103cb27fad6ac
Quinn Edenfield <quinn@qeden.dev>
2026-06-21 17:01:03 -0700

    feat: implement main (cmd) package

--- pkgs/vm-runner-go/cmd/virby-vm/main.go
+++ pkgs/vm-runner-go/cmd/virby-vm/main.go
@@ -1,3 +1,37 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"os"
+	"vm-runner/internal/config"
+	"vm-runner/internal/runner"
+	"vm-runner/internal/signalmanager"
+)
+
+func run() int {
+	log.SetFlags(log.LstdFlags)
+	log.SetOutput(os.Stdout)
+
+	configFilePath := os.Getenv("VIRBY_VM_CONFIG_FILE")
+	cfg, err := config.NewVMConfig(configFilePath)
+	if err != nil {
+		log.Print(err)
+		return 1
+	}
+
+	sm := signalmanager.NewSignalManager()
+	sm.Setup()
+	defer sm.Cleanup()
+
+	r := runner.NewRunner(cfg, sm)
+	if err := r.Run(); err != nil {
+		log.Print(err)
+		return 1
+	}
+
+	return 0
+}
+
+func main() {
+	os.Exit(run())
+}
